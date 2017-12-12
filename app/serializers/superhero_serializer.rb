977787eb203674014bc51e049d5ee87e1a2567fb
Cara Bean <carabean@Odysseus.local>
2017-12-12 17:27:57 -0500

    we're getting so good at this

--- app/serializers/superhero_serializer.rb
+++ app/serializers/superhero_serializer.rb
@@ -1,12 +1,12 @@
 class SuperheroSerializer < ActiveModel::Serializer
   attributes :id,
-    :name,
-    :superpower,
-    :backstory,
-    :image_url,
-    :user,
-    :reviews,
-    :current_user
+             :name,
+             :superpower,
+             :backstory,
+             :image_url,
+             :user,
+             :reviews,
+             :current_user
 
   def current_user
     scope.current_user
