8dbe74e3d9f60a630f8ff472b0d4b71764c5894a
Antonio Pugliese <apugliese74@gmail.com>
2017-12-14 13:57:33 -0500

    resolved conflicts

--- spec/controllers/api/v1/superheroes_controller_spec.rb
+++ spec/controllers/api/v1/superheroes_controller_spec.rb
@@ -76,17 +76,15 @@ RSpec.describe Api::V1::SuperheroesController, type: :controller do
     it 'posts a single hero' do
       sign_in kjoya
 
-      params =
-      {
-        superhero:
-          {
+      params = {
+        superhero: {
             name: 'daredevil',
             backstory: 'blind and stuff',
             superpower: 'ninja skills',
             image_url: 'image.png',
             user_id: kjoya.id
           }
-      }
+        }
 
       expect { post :create, params: params }.to change(Superhero, :count).by(1)
     end
